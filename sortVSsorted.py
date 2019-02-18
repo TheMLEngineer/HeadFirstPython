dc0d670876d5147e5f30a8ecc8e33ed4db1789d3
Karthikeyan Muthu <phenomenalkarthikeyan@gmail.com>
2019-02-19 02:01:23 +0530

    Add files via upload

---
+++ sortVSsorted.py
@@ -0,0 +1,10 @@
+a = [213,45,76123,7,23,56,34]
+print("Starting values\n",a) 
+a.sort()
+print("Sort()\n",a)
+
+a = [213,45,76123,7,23,56,34]
+print("Starting values\n",a)
+b = sorted(a)
+#cREATES A COPY OF SORTED list
+print("Sorted()\n",b)
